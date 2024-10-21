50c404bd772dd65402c34152b1b29544f244668e
Kirill Puljavin <105564583+KirillPuljavin@users.noreply.github.com>
2024-10-21 23:30:48 +0200

    Update

--- Fit_Track_App/Fit_Track_App/obj/Debug/net8.0-windows/Pages/RegisterAccountPage.g.i.cs
+++ Fit_Track_App/Fit_Track_App/obj/Debug/net8.0-windows/Pages/RegisterAccountPage.g.i.cs
@@ -53,7 +53,7 @@ namespace Fit_Track_App.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Fit_Track_App;V1.0.0.0;component/pages/registeraccountpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Fit_Track_App;component/pages/registeraccountpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\RegisterAccountPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Fit_Track_App/Fit_Track_App/obj/Debug/net8.0-windows/Pages/StartPage.g.i.cs
+++ Fit_Track_App/Fit_Track_App/obj/Debug/net8.0-windows/Pages/StartPage.g.i.cs
@@ -53,7 +53,7 @@ namespace Fit_Track_App.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Fit_Track_App;V1.0.0.0;component/pages/startpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Fit_Track_App;component/pages/startpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\StartPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
