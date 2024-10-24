83f160ac434369eef39db1278558c1f416de2e98
SFG <kirill.puljavin@gmail.com>
2024-10-24 10:24:38 +0200

    project revision

---
+++ Fit_Track_App/Fit_Track_App/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Fit_Track_App.Pages;
+using mvvm_genomgång.MVVM;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Fit_Track_App.ViewModels
+{
+    public class StartPageViewModel : ViewModelBase
+    {
+        public ICommand SignInCommand { get; }
+        public ICommand LogInCommand { get; }
+
+        public StartPageViewModel()
+        {
+            SignInCommand = new RelayCommand(OnSignIn);
+            LogInCommand = new RelayCommand(OnLogIn);
+        }
+
+        private void OnSignIn(object parameter)
+        {
+            if (Application.Current.MainWindow is MainWindow window)
+            {
+                window.MainFrame.Navigate(new RegisterAccountPage());
+            }
+        }
+
+        private void OnLogIn(object parameter)
+        {
+            // Logic to handle Log In
+        }
+    }
+}
