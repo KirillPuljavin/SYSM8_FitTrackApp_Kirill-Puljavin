bccddb41ccde4654eb41f7f1ef5ac26443793561
Kirill Puljavin <105564583+KirillPuljavin@users.noreply.github.com>
2024-10-25 15:45:15 +0200

    restructured project

---
+++ Fit_Track_App/Classes/ViewModelHelpers.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Fit_Track_App.Classes
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        private Func<bool> login;
+        private Action register;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Func<bool> login)
+        {
+            this.login = login;
+        }
+
+        public RelayCommand(Action register)
+        {
+            this.register = register;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
